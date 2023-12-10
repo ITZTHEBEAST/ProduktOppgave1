bb1fcfc79b7bb514e19193865c038d0aa89eedf1
Fredrik <60442942+ITZTHEBEAST@users.noreply.github.com>
2023-12-10 23:00:46 +0100

    Add files via upload
    :o

---
+++ ProduktOppgave1/src/test/java/com/example/produktoppgave1/ProduktOppgave1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.produktoppgave1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProduktOppgave1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
